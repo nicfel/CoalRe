23f95c31971fa2f98b21466ed0700f159f46e065
Tim Vaughan <tgvaughan@gmail.com>
2018-05-17 15:02:21 +0200

    Added density test.

--- test/coalre/distribution/CoalescentWithReassortmentTest.java
+++ test/coalre/distribution/CoalescentWithReassortmentTest.java
@@ -4,6 +4,7 @@ import beast.core.parameter.RealParameter;
 import beast.evolution.tree.coalescent.ConstantPopulation;
 import coalre.CoalReTestClass;
 import coalre.network.Network;
+import junit.framework.Assert;
 import org.junit.Test;
 
 public class CoalescentWithReassortmentTest extends CoalReTestClass {
@@ -11,7 +12,11 @@ public class CoalescentWithReassortmentTest extends CoalReTestClass {
     @Test
     public void testDensity() {
 
-        Network network = new Network("(((A[&segments={0,1,2,3,4,5,6,7}]:1)#1[&segments={1,4}]:1,B[&segments={0,1,2,3,4,5,6,7}]:2)[&segments={0,1,2,3,4,5,6,7}]:1,#1[&segments={0,2,3,5,6,7}]:2)[&segments={0,1,2,3,4,5,6,7}]:0.0;");
+        Network network = new Network(
+                "(((A[&segments={0,1,2,3,4,5,6,7}]:1)#1[&segments={1,4}]:1," +
+                        "B[&segments={0,1,2,3,4,5,6,7}]:2)[&segments={0,1,2,3,4,5,6,7}]:1," +
+                        "#1[&segments={0,2,3,5,6,7}]:2)[&segments={0,1,2,3,4,5,6,7}]:0.0;");
+
         NetworkIntervals networkIntervals = new NetworkIntervals();
         networkIntervals.initByName("network", network);
 
@@ -23,7 +28,6 @@ public class CoalescentWithReassortmentTest extends CoalReTestClass {
                 "reassortmentRate", new RealParameter("1.0"),
                 "populationModel", populationFunction);
 
-        System.out.println(coalWR.calculateLogP());
-
+        Assert.assertEquals(-16.258280263919616, coalWR.calculateLogP(), 1e-10);
     }
 }
