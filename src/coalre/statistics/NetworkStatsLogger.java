a12ec8eb2f762569a72c5ca5e36a88352cb98cca
Nicola Müller <nicola.felix.mueller@gmail.com>
2019-01-04 11:08:39 +0100

    added loggers for join and split reassortment count and for stats w/o the sampling form the prior part

--- src/coalre/statistics/NetworkStatsLogger.java
+++ src/coalre/statistics/NetworkStatsLogger.java
@@ -3,11 +3,16 @@ package coalre.statistics;
 import beast.core.BEASTObject;
 import beast.core.Input;
 import beast.core.Loggable;
+import beast.evolution.tree.Tree;
 import coalre.network.Network;
 import coalre.network.NetworkEdge;
 import coalre.network.NetworkNode;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class NetworkStatsLogger extends BEASTObject implements Loggable {
 
@@ -15,14 +20,22 @@ public class NetworkStatsLogger extends BEASTObject implements Loggable {
     public Input<Network> networkInput = new Input<>("network",
             "Network for which to log statistics.",
             Input.Validate.REQUIRED);
+    
+    public Input<List<Tree>> segmentTreesInput = new Input<>("segmentTree",
+            "Segment tree associated with network.",
+            new ArrayList<>());
+
 
     Network network;
+    boolean logObservable = false;
 
     public NetworkStatsLogger() { }
 
     @Override
     public void initAndValidate() {
         network = networkInput.get();
+        if(segmentTreesInput.get().size()>0)
+        	logObservable = true;
     }
 
     @Override
@@ -30,17 +43,34 @@ public class NetworkStatsLogger extends BEASTObject implements Loggable {
 
         String prefix = network.getID() == null ? "networkStat." : network.getID() + ".";
 
-        out.print(prefix + "height\t" +
-                prefix + "totalLength\t" +
-                prefix + "reassortmentNodeCount\t");
+        if (logObservable)
+	        out.print(prefix + "obsHeight\t" +
+	                prefix + "obsTotalLength\t" +
+	                prefix + "obsReassortmentNodeCount\t");
+        else
+        	out.print(prefix + "height\t" +
+	                prefix + "totalLength\t" +
+	                prefix + "reassortmentNodeCount\t");
+
     }
 
     @Override
     public void log(long sample, PrintStream out) {
+        if (logObservable){
+    		double[] rootHeights = new double[segmentTreesInput.get().size()];
+			for (int i = 0; i < segmentTreesInput.get().size(); i++)
+				rootHeights[i] = segmentTreesInput.get().get(i).getRoot().getHeight();
+			
+        	out.print(getTotalHeight(network, rootHeights) + "\t" +
+	                getTotalEdgeLength(network, rootHeights) + "\t" +
+	                getReassortmentCount(network, rootHeights) + "\t");
 
-        out.print(getTotalHeight(network) + "\t" +
-                getTotalEdgeLength(network) + "\t" +
-                getReassortmentCount(network) + "\t");
+        	
+        }else{   
+        	out.print(getTotalHeight(network) + "\t" +
+	                getTotalEdgeLength(network) + "\t" +
+	                getReassortmentCount(network) + "\t");
+        }
     }
 
     @Override
@@ -60,4 +90,49 @@ public class NetworkStatsLogger extends BEASTObject implements Loggable {
     public static double getTotalHeight(Network network) {
         return network.getRootEdge().childNode.getHeight();
     }
+    
+    public static int getReassortmentCount(Network network, double[] rootHeights) {
+    	double maxHeight = 0.0;
+    	for (int i = 0; i < rootHeights.length; i++)
+    		if (rootHeights[i] > maxHeight)
+    			maxHeight = rootHeights[i];
+    	
+    	final double finalMaxHeight = maxHeight;
+
+        return (int)network.getNodes().stream()
+        		.filter(NetworkNode::isReassortment)
+        		.filter(n -> n.getHeight() < finalMaxHeight)
+        		.count();
+    }
+
+    public static double getTotalEdgeLength(Network network, double[] rootHeights) {
+    	double totalLength = 0.0;
+    	List<NetworkEdge> networkEdges = new ArrayList<>(network.getEdges());
+        List<NetworkEdge> nonRootEdges = networkEdges.stream()
+        		.filter(e -> !e.isRootEdge())
+                .collect(Collectors.toList());
+        // check for each edge if it has at least one segment for which the root hasn't been reched yet
+        for (int i = 0; i < nonRootEdges.size(); i++){
+        	double childHeight = nonRootEdges.get(i).childNode.getHeight();
+    		final BitSet hasSegment = nonRootEdges.get(i).hasSegments;
+    		for (int j = 0; j < rootHeights.length; j++){
+    			if (hasSegment.get(j) && childHeight < rootHeights[j]){
+    				totalLength += nonRootEdges.get(i).parentNode.getHeight() - childHeight;
+    				break;
+    			}
+    		}
+        	
+        }        	
+    	
+        return totalLength;
+    }
+
+    public static double getTotalHeight(Network network, double[] rootHeights) {
+    	double maxHeight = 0.0;
+    	for (int i = 0; i < rootHeights.length; i++)
+    		if (rootHeights[i] > maxHeight)
+    			maxHeight = rootHeights[i];
+        return maxHeight;
+    }
+
 }
---
+++ src/coalre/statistics/ReassortmentStatLogger.java
@@ -0,0 +1,130 @@
+package coalre.statistics;
+
+import beast.core.BEASTObject;
+import beast.core.Input;
+import beast.core.Loggable;
+import beast.evolution.tree.Tree;
+import coalre.network.Network;
+import coalre.network.NetworkEdge;
+import coalre.network.NetworkNode;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReassortmentStatLogger extends BEASTObject implements Loggable {
+
+
+    public Input<Network> networkInput = new Input<>("network",
+            "Network for which to log statistics.",
+            Input.Validate.REQUIRED);
+    
+    public Input<List<Tree>> segmentTreesInput = new Input<>("segmentTree",
+            "Segment tree associated with network.",
+            new ArrayList<>());
+
+
+    Network network;
+    int segCount;
+    boolean logObservable = false;
+
+
+    public ReassortmentStatLogger() { }
+
+    @Override
+    public void initAndValidate() {
+        network = networkInput.get();
+        segCount = network.getSegmentCount();
+        
+        if(segmentTreesInput.get().size()>0)
+        	logObservable = true;
+
+    }
+
+    @Override
+    public void init(PrintStream out) {
+        String prefix = network.getID() == null ? "networkStat." : network.getID() + ".";
+
+		if (logObservable){
+	        for (int i = 0; i < segCount; i++){
+	        	for (int j = i+1; j < segCount; j++){
+	                out.print(prefix + "jointReassortment." + i + "_" + j + "\t");        		
+	                out.print(prefix + "splitReassortment." + i + "_" + j + "\t");        		
+	        	}
+	        }    
+		}else{
+	        for (int i = 0; i < segCount; i++){
+	        	for (int j = i+1; j < segCount; j++){
+	                out.print(prefix + "jointObsReassortment." + i + "_" + j + "\t");        		
+	                out.print(prefix + "splitObsReassortment." + i + "_" + j + "\t");        		
+	        	}
+	        }    
+
+		}
+		
+    }
+
+    @Override
+    public void log(long sample, PrintStream out) {
+    	int[][] jointReassortmentCount = new int[segCount][segCount];
+    	int[][] splitReassortmentCount = new int[segCount][segCount];
+    	
+		double[] rootHeights = new double[segCount];
+		if (logObservable)
+			for (int i = 0; i < segCount; i++)
+				rootHeights[i] = segmentTreesInput.get().get(i).getRoot().getHeight();
+		else
+			for (int i = 0; i < segCount; i++)
+				rootHeights[i] = Double.POSITIVE_INFINITY;
+
+
+    	
+		List<NetworkNode> networkNodes = new ArrayList<>(network.getNodes());
+
+    	final List<NetworkNode> reassortmentNodes = networkNodes.stream()
+    			.filter(NetworkNode::isReassortment)
+    			.collect(Collectors.toList());
+    	
+    	for (int k = 0; k < reassortmentNodes.size(); k++){
+    		List<NetworkEdge> parentEdges = reassortmentNodes.get(k).getParentEdges();
+    		List<NetworkEdge> childEdge = reassortmentNodes.get(k).getChildEdges();
+    		
+    		final BitSet childSegment = childEdge.get(0).hasSegments;
+    		final BitSet parent1Segments = parentEdges.get(0).hasSegments;
+    		final BitSet parent2Segments = parentEdges.get(1).hasSegments;
+    		
+    		// count which edges go where
+            for (int i = 0; i < segCount; i++){
+            	if (childSegment.get(i) && reassortmentNodes.get(k).getHeight()<rootHeights[i]){
+	            	for (int j = i+1; j < segCount; j++){
+	            		if(childSegment.get(j) && reassortmentNodes.get(k).getHeight()<rootHeights[j]){
+	            			if (parent1Segments.get(i) && parent1Segments.get(j)){
+	            				jointReassortmentCount[i][j]++;
+	            			}else if(parent2Segments.get(i) && parent2Segments.get(j)){
+	            				jointReassortmentCount[i][j]++;
+	            			}else{
+	            				splitReassortmentCount[i][j]++;
+	            			}	            			
+	            		}
+	            	}
+            	}
+            } 
+    		
+    	}
+    	
+        for (int i = 0; i < segCount; i++){
+        	for (int j = i+1; j < segCount; j++){
+                out.print(jointReassortmentCount[i][j]+ "\t" +
+                		splitReassortmentCount[i][j]+ "\t");
+        	}
+        } 
+	}
+
+    @Override
+    public void close(PrintStream out) {
+
+    }
+}
