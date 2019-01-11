bae7275689348c97eb62b1456c0ed5cf91974682
Tim Vaughan <tgvaughan@gmail.com>
2019-01-11 16:32:45 +0100

    Added Function enabling spike+slab priors.

---
+++ src/coalre/util/SpikeSlabParameter.java
@@ -0,0 +1,59 @@
+package coalre.util;
+
+import beast.core.BEASTObject;
+import beast.core.Function;
+import beast.core.Input;
+import beast.core.parameter.BooleanParameter;
+import beast.core.parameter.RealParameter;
+
+public class SpikeSlabParameter extends BEASTObject implements Function {
+
+    public Input<BooleanParameter> indicatorInput = new Input<>("indicator",
+            "Boolean parameter indicating which elements are spike and which are slab.",
+            Input.Validate.REQUIRED);
+
+    public Input<Function> spikeValuesInput = new Input<>("spikeValues",
+            "Value of each element when in spike mode.",
+            Input.Validate.REQUIRED);
+
+    public Input<Function> slabValuesInput = new Input<>("slabValues",
+            "Value of each element when in slab mode.",
+            Input.Validate.REQUIRED);
+
+    Function spikeValues, slabValues;
+    BooleanParameter indicators;
+
+    SpikeSlabParameter() { }
+
+    @Override
+    public void initAndValidate() {
+
+        indicators = indicatorInput.get();
+        spikeValues = spikeValuesInput.get();
+        slabValues = slabValuesInput.get();
+
+        if (indicators.getDimension() != spikeValues.getDimension()
+                || indicators.getDimension() != slabValues.getDimension()) {
+            throw new IllegalArgumentException("Dimensions of all inputs to " +
+                    "SpikeSlabParameter must match.");
+        }
+
+    }
+
+    @Override
+    public int getDimension() {
+        return indicators.getDimension();
+    }
+
+    @Override
+    public double getArrayValue(int i) {
+        return indicators.getValue(i)
+                ? spikeValues.getArrayValue(i)
+                : slabValues.getArrayValue(i);
+    }
+
+    @Override
+    public double getArrayValue() {
+        return getArrayValue(0);
+    }
+}
