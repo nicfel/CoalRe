142add73d90e21b6d6d37ad3e1f3f86c4af73e77
Nicola Müller <nicola.felix.mueller@gmail.com>
2019-04-11 15:08:31 +0200

    implements weighted sum distributions
    Allows to specify a distribution as a weighted sum of many distributions (mostly for tip date estimation where the dates of some of the tips aren't fully trustworthy)

---
+++ src/coalre/util/WeightedSumDistribution.java
@@ -0,0 +1,82 @@
+package coalre.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.math.MathException;
+import org.apache.commons.math.distribution.ContinuousDistribution;
+import org.apache.commons.math.distribution.Distribution;
+
+import beast.core.Description;
+import beast.core.Function;
+import beast.core.Input;
+import beast.core.Input.Validate;
+import beast.core.parameter.RealParameter;
+import beast.math.distributions.ParametricDistribution;
+import beast.util.Randomizer;
+
+
+
+@Description("Dirichlet distribution.  p(x_1,...,x_n;alpha_1,...,alpha_n) = 1/B(alpha) prod_{i=1}^K x_i^{alpha_i - 1} " +
+        "where B() is the beta function B(alpha) = prod_{i=1}^K Gamma(alpha_i)/ Gamma(sum_{i=1}^K alpha_i}. ")
+public class WeightedSumDistribution extends ParametricDistribution {
+    public final Input<List<ParametricDistribution>> distInput = new Input<>("distr",
+            "distribution used to calculate prior over MRCA time, "
+                    + "e.g. normal, beta, gamma. If not specified, monophyletic must be true", new ArrayList<>());
+	
+    final public Input<RealParameter> weightsInput = new Input<>("weights", "weighting of the individual distrbutions ", Validate.REQUIRED);
+
+    RealParameter weights;
+    
+    List<ParametricDistribution> distributions;
+    
+    @Override
+    public void initAndValidate() {
+    	weights = weightsInput.get();
+    	distributions = distInput.get();
+    	
+    	if (weights.getDimension()!=distributions.size())
+    		throw new IllegalArgumentException("the number of weights given differs from the number of distribution");
+    	    	
+    }
+
+    @Override
+    public Distribution getDistribution() {
+        return null;
+    }
+
+    @Override
+    public double calcLogP(Function pX) {
+        double logP = 0;
+        for (int i = 0; i < pX.getDimension(); i++) {
+            double x = pX.getArrayValue(i);
+            double prob = 0;
+            for (int j = 0; j < weights.getDimension(); j++){
+            	try {
+					prob += weights.getArrayValue(j)*distributions.get(j).cumulativeProbability(x);
+				} catch (MathException e) {
+					System.out.println(e);
+				}
+            }
+            logP += Math.log(prob);
+        }
+        return logP;
+    }
+
+    @Override
+    public double logDensity(double val){
+        double logP = 0;
+        double x = val;
+        double prob = 0;
+        for (int j = 0; j < weights.getDimension(); j++){
+			prob += weights.getArrayValue(j)*distributions.get(j).density(x);
+        }
+        logP += Math.log(prob);
+        return logP;
+    }
+    
+	@Override
+	public Double[][] sample(int size) {
+		return null;
+	}
+}
